ae767371b207a4d525816c5ba3bb6dd274b55ef8
Giovanni De Luca <ginholucacorrea@icloud.com>
2020-08-04 18:16:19 -0300

    caule

---
+++ src/components/PageHeader/index.tsx
@@ -0,0 +1,5 @@
+import Reacr from 'react'
+
+function PageHeader() {
+  
+}
---
+++ src/pages/TeacherForm/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TeacherForm() {
+  return (
+    <h1>Caule</h1>
+  )
+}
+
+export default TeacherForm
