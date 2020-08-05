09ff8e4dc800b3544c3899ea2de94291e879129b
Giovanni De Luca <ginholucacorrea@icloud.com>
2020-08-04 22:46:59 -0300

    caule

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,5 +1,28 @@
-import Reacr from 'react'
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-function PageHeader() {
-  
+import logoImg from '../../assets/images/logo.svg'
+import backIcon from '../../assets/images/icons/back.svg'
+
+interface PageHeaderProps {
+  title: string
 }
+
+const PageHeader = () => {
+  return (
+    <header className="page-header">
+        <div className="top-bar-container">
+          <Link to="/">
+            <img src={backIcon} alt="Voltar"/>
+          </Link>
+          <img src={logoImg} alt="Proffy"/>
+        </div>
+
+        <div className="header-content">
+          <strong>Estes são os proffys disponíveis.</strong>
+        </div>
+      </header>
+  )
+} 
+
+export default PageHeader
